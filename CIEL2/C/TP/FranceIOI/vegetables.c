79b6caa78d6ec2f1d5d1f4bd0d768075da9bc9c4
Thomas Le Goff <thomaslegoff56@laposte.net>
2025-09-02 12:04:27 +0200

    add CIEL2 introduction

---
+++ CIEL2/C/TP/FranceIOI/vegetables.c
@@ -0,0 +1,22 @@
+#include "stdio.h"
+
+int main( int argc, char * argv[] )
+{
+   int nbr_legumes = 0;
+   int i = 0;
+
+   scanf("%d", &nbr_legumes);   
+
+   while(i < nbr_legumes) {
+      double poid = 0;
+      double age = 0;
+      double prix = 0;
+      
+      scanf("%lf", &poid);
+      scanf("%lf", &age);
+      scanf("%lf", &prix);
+      
+      printf("%lf \n", prix/poid);
+      i++;
+   }
+}
