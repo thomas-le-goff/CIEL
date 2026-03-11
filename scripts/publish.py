70cf4eb5cfda41b8c7fd42636a49ad8ff60c9a8e
Thomas Le Goff <thomaslegoff56@laposte.net>
2026-03-11 13:39:23 +0100

    feat: handle metadata for courses publication and be able to ignore files

--- scripts/publish.py
+++ scripts/publish.py
@@ -3,44 +3,103 @@ import os
 import json
 import shutil
 import subprocess
+import yaml
 
+from abc import ABC, abstractmethod
 from pathlib import Path
 
 
-def extract_export_type_from_name(file_name):
-    splitted = file_name.split(".")
-    if len(splitted) < 3:
+class AbstractFileHandler(ABC):
+
+    @abstractmethod
+    def setup(self, output_dir: Path) -> None:
+        pass
+
+    @abstractmethod
+    def teardown(self) -> None:
+        pass
+
+    @abstractmethod
+    def match(self, file_path: Path) -> bool:
+        pass
+
+    @abstractmethod
+    def handle(self, file_src_path: Path, file_dst_path: Path) -> Path | None:
+        pass
+
+
+class ChapterMetadataFileHandler(AbstractFileHandler):
+
+    def __init__(self) -> None:
+        super().__init__()
+        self._metadata = {}
+        self._metadatafile: Path | None = None
+
+    def setup(self, output_dir: Path) -> None:
+        self._metadatafile = output_dir / "_data" / "metadata.json"
+
+    def teardown(self) -> None:
+        if self._metadatafile is None:
+            return
+        with open(self._metadatafile, "w") as output_file:
+            output_file.write(json.dumps(self._metadata, ensure_ascii=False))
+
+    def match(self, file_path: Path) -> bool:
+        return file_path.match(".metadata.yml")
+
+    def handle(self, file_src_path: Path, file_dst_path: Path) -> Path | None:
+        with open(file_src_path, mode='r') as f:
+            #  Todo: find a better way of doing this
+            id = str(file_src_path.parent).removeprefix("../src/")
+            self._metadata[id] = yaml.safe_load(f) or {}
         return None
-    return splitted[-2]
 
 
-def publish_file(file_src_path, file_dst_path, export_type):
-    publish_file_path = file_dst_path
-    match export_type:
-        case None:
-            return None
-        case "copy":
-            shutil.copyfile(file_src_path, publish_file_path)
-        case "pdf-slideshow":
-            publish_file_path = file_dst_path.with_suffix(".pdf")
-            subprocess.run(
-                [
-                    "npx",
-                    "@marp-team/marp-cli@latest",
-                    "--allow-local-files",
-                    "--pdf",
-                    file_src_path,
-                    "--output",
-                    publish_file_path,
-                ]
-            )
-        # Todo add pandoc case
-        case _:
-            raise RuntimeError(f"Unknown export type : {export_type}")
-    return publish_file_path
+class MarplePdfSlideShowFileHandler(AbstractFileHandler):
+
+    def setup(self, output_dir: Path) -> None:
+        pass
+
+    def teardown(self) -> None:
+        pass
+
+    def match(self, file_path: Path) -> bool:
+        return file_path.match("*.pdf-slideshow.md")
+
+    def handle(self, file_src_path: Path, file_dst_path: Path) -> Path:
+        publish_file_path = file_dst_path.with_suffix(".pdf")
+        subprocess.run(
+            [
+                "npx",
+                "@marp-team/marp-cli@latest",
+                "--allow-local-files",
+                "--pdf",
+                file_src_path,
+                "--output",
+                publish_file_path,
+            ]
+        )
+
+        return publish_file_path
 
 
-if __name__ == "__main__":
+class CopyFileHandler(AbstractFileHandler):
+
+    def setup(self, output_dir: Path) -> None:
+        pass
+
+    def teardown(self) -> None:
+        pass
+
+    def match(self, file_path: Path) -> bool:
+        return True
+
+    def handle(self, file_src_path: Path, file_dst_path: Path) -> Path:
+        shutil.copyfile(file_src_path, file_dst_path)
+        return file_dst_path
+
+
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-o", "--output", required=True)
 
@@ -53,28 +112,48 @@ if __name__ == "__main__":
 
     tree = {}
     root_dir = Path("../src")
-    file_formats = ".md"
 
     attachments_dir = args.output / "attachments"
     if attachments_dir.exists():
         shutil.rmtree(attachments_dir)
+
+    file_handlers: list[AbstractFileHandler] = [
+        ChapterMetadataFileHandler(),
+        MarplePdfSlideShowFileHandler(),
+        CopyFileHandler(),  #  Keep it last
+    ]
+
+    for fh in file_handlers:
+        fh.setup(args.output)
+
+    def handle_file(final_src_path: Path, final_dst_path: Path):
+        if Path.is_file(final_src_path):
+            for fh in file_handlers:
+                if fh.match(final_src_path):
+                    p_file = fh.handle(final_src_path, final_dst_path)
+                    if p_file:
+                        tree[d][sd].append(p_file.name)
+                    break
 
     for d in os.listdir(root_dir):
         tree[d] = {}
         for sd in os.listdir(root_dir / d):
             final_src_path = root_dir / d / sd
             final_dst_path = attachments_dir / d / sd
-            os.makedirs(final_dst_path)
-            tree[d][sd] = []
-            for f in os.listdir(final_src_path):
-                if Path.is_file(final_src_path / f) and f.endswith(file_formats):
-                    p_file = publish_file(
-                        final_src_path / f,
-                        final_dst_path / f,
-                        export_type=extract_export_type_from_name(f),
-                    )
-                    if p_file:
-                        tree[d][sd].append(p_file.name)
+            if final_src_path.is_dir():
+                os.makedirs(final_dst_path)
+                tree[d][sd] = []
+                for f in os.listdir(final_src_path):
+                    handle_file(final_src_path / f, final_dst_path / f)
+            else:
+                handle_file(final_src_path, final_dst_path)
+
+    for fh in file_handlers:
+        fh.teardown()
 
     with open(args.output / "_data" / "attachments.json", "w") as output_file:
         output_file.write(json.dumps(tree, ensure_ascii=False))
+
+
+if __name__ == "__main__":
+    main()
